6889060962a47c544b2c7d527da30c8e5056d2cd
lezhongwen <46175757+lezhongwen@users.noreply.github.com>
2023-08-28 19:13:04 +0800

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -25,53 +25,53 @@ const tools = {
     "hitokoto": {
         icon: fa_comment,
         callback: showHitokoto
-    },
-    "asteroids": {
-        icon: fa_paper_plane,
-        callback: () => {
-            if (window.Asteroids) {
-                if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
-                window.ASTEROIDSPLAYERS.push(new Asteroids());
-            } else {
-                const script = document.createElement("script");
-                script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
-                document.head.appendChild(script);
-            }
-        }
-    },
-    "switch-model": {
-        icon: fa_user_circle,
-        callback: () => {}
-    },
-    "switch-texture": {
-        icon: fa_street_view,
-        callback: () => {}
-    },
-    "photo": {
-        icon: fa_camera_retro,
-        callback: () => {
-            showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
-            Live2D.captureName = "photo.png";
-            Live2D.captureFrame = true;
-        }
-    },
-    "info": {
-        icon: fa_info_circle,
-        callback: () => {
-            open("https://github.com/stevenjoezhang/live2d-widget");
-        }
-    },
-    "quit": {
-        icon: fa_xmark,
-        callback: () => {
-            localStorage.setItem("waifu-display", Date.now());
-            showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
-            document.getElementById("waifu").style.bottom = "-500px";
-            setTimeout(() => {
-                document.getElementById("waifu").style.display = "none";
-                document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
-            }, 3000);
-        }
+    }
+    // "asteroids": {
+    //     icon: fa_paper_plane,
+    //     callback: () => {
+    //         if (window.Asteroids) {
+    //             if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
+    //             window.ASTEROIDSPLAYERS.push(new Asteroids());
+    //         } else {
+    //             const script = document.createElement("script");
+    //             script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
+    //             document.head.appendChild(script);
+    //         }
+    //     }
+    // },
+    // "switch-model": {
+    //     icon: fa_user_circle,
+    //     callback: () => {}
+    // },
+    // "switch-texture": {
+    //     icon: fa_street_view,
+    //     callback: () => {}
+    // },
+    // "photo": {
+    //     icon: fa_camera_retro,
+    //     callback: () => {
+    //         showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
+    //         Live2D.captureName = "photo.png";
+    //         Live2D.captureFrame = true;
+    //     }
+    // },
+    // "info": {
+    //     icon: fa_info_circle,
+    //     callback: () => {
+    //         open("https://github.com/stevenjoezhang/live2d-widget");
+    //     }
+    // },
+    // "quit": {
+    //     icon: fa_xmark,
+    //     callback: () => {
+    //         localStorage.setItem("waifu-display", Date.now());
+    //         showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
+    //         document.getElementById("waifu").style.bottom = "-500px";
+    //         setTimeout(() => {
+    //             document.getElementById("waifu").style.display = "none";
+    //             document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
+    //         }, 3000);
+    //     }
     }
 };
 
